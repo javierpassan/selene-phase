460c08d723be7ea9c9006c2a06778134e24bf9d0
Javier Pastor Sánchez <javierpassan@gmail.com>
2022-12-25 19:28:27 +0100

    sytle: format with spaces

--- functions/PostWebhookNotificationsTelegram.js
+++ functions/PostWebhookNotificationsTelegram.js
@@ -2,12 +2,12 @@ const logger = console;
 
 exports = async function (request, response) {
   try {
-    if(request.body === undefined) {
+    if (request.body === undefined) {
       throw new Error(`Request body was not defined.`)
     }
-    
+
     const body = JSON.parse(request.body.text());
-  
+
     logger.log(JSON.stringify(body));
 
     response.setStatusCode(204);
