8cd9300e78f0137bb3757c3685eaa3a790df58ab
Javier Pastor Sánchez <javierpassan@gmail.com>
2022-12-25 19:33:22 +0100

    refactor: set response body

--- functions/PostWebhookNotificationsTelegram.js
+++ functions/PostWebhookNotificationsTelegram.js
@@ -3,17 +3,25 @@ const logger = console;
 exports = async function (request, response) {
   try {
     if (request.body === undefined) {
-      throw new Error(`Request body was not defined.`)
+      throw new Error('Request body was not defined.');
     }
 
     const body = JSON.parse(request.body.text());
 
     logger.log(JSON.stringify(body));
 
-    response.setStatusCode(204);
-    response.setBody();
+    response.setStatusCode(200);
+    response.setBody(JSON.stringify({
+      success: {
+        message: 'Event processed successfully.',
+      },
+    }));
   } catch (error) {
     response.setStatusCode(400);
-    response.setBody(error.message);
+    response.setBody({
+      error: {
+        message: error.message,
+      },
+    });
   }
 }
